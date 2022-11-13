0c7ce5032bca7cc3343305ebd1f4833bd2caae11
Aura Husnaini P.Z <78643049+auraahsn@users.noreply.github.com>
2022-11-13 15:31:02 +0700

    cust_dashboard_portofolio

--- app/Views/customer/dashboard.php
+++ app/Views/customer/dashboard.php
@@ -11,10 +11,71 @@
     </div>
 
     <div class="section-body">
+        <!-- START DASHBOARD-1 -->
+        <div class="row">
+            <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                <div class="card card-statistic-1">
+                    <div class="card-icon bg-primary">
+                        <i class="far fa-circle"></i>
+                    </div>
+                    <div class="card-wrap">
+                        <div class="card-header">
+                            <h4>Pending</h4>
+                        </div>
+                        <div class="card-body">
+                            <?= hitungData('pemesanan') ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                <div class="card card-statistic-1">
+                    <div class="card-icon bg-danger">
+                        <i class="far fa-newspaper"></i>
+                    </div>
+                    <div class="card-wrap">
+                        <div class="card-header">
+                            <h4>On Progress</h4>
+                        </div>
+                        <div class="card-body">
+                            <?= hitungData('events') ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                <div class="card card-statistic-1">
+                    <div class="card-icon bg-warning">
+                        <i class="far fa-file"></i>
+                    </div>
+                    <div class="card-wrap">
+                        <div class="card-header">
+                            <h4>Selesai</h4>
+                        </div>
+                        <div class="card-body">
+                            <?= hitungData('portofolio') ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                <div class="card card-statistic-1">
+                    <div class="card-icon bg-success">
+                        <i class="fas fa-user"></i>
+                    </div>
+                    <div class="card-wrap">
+                        <div class="card-header">
+                            <h4>Admin</h4>
+                        </div>
+                        <div class="card-body">
+                            <?= hitungData('users') ?>
+                        </div>
+                    </div>
+                    <!-- END DASHBOARD-1 -->
+                </div>
+            </div>
+        </div>
+    </div>
 
-    </div>
-    </div>
-    </div>
-    </div>
 </section>
 <?= $this->endSection() ?>
