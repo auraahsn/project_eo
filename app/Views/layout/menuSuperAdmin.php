19ad3fe266ada33d7dac77383e1f6003d0c8353d
Aura Husnaini P.Z <78643049+auraahsn@users.noreply.github.com>
2022-11-22 04:41:08 +0700

    menu_superadmin

--- app/Views/layout/menuSuperAdmin.php
+++ app/Views/layout/menuSuperAdmin.php
@@ -1,5 +1,5 @@
 <li class="menu-header">Main Menu</li>
-<li><a class="nav-link" href="<?= site_url('home') ?>"><i class="fas fa-home"></i> <span>Data User</span></a></li>
+<li><a class="nav-link" href="<?= site_url('homesuperadmin') ?>"><i class="fas fa-home"></i> <span>Data User</span></a></li>
 <!-- <li><a class="nav-link" href="<?= site_url('about') ?>"><i class="fas fa-heart"></i> <span>About</span></a></li> -->
 <!-- <li class="nav-item dropdown">
     <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>
